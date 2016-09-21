280b34c9665dcc706f79dd49af393fe03b1e8e94
Daniel <daniel.sonner@gmail.com>
2016-09-21 13:46:24 -0700

    Fibonacci done

--- src/main/scala/exercises/02_Fibonacci.scala
+++ src/main/scala/exercises/02_Fibonacci.scala
@@ -1,13 +1,20 @@
 package exercises
 
 object Fibonacci {
-  
-  /**
-   * Implement the classic Fibonacci sequence, where
-   *    fib(1) = 1
-   *    fib(2) = 1
-   *    fib(n) = fib(n-1) + fib(n-2)
-   */
-  def fib(n: Int): Int = 0
+
+	/**
+	 * Implement the classic Fibonacci sequence, where
+	 *    fib(1) = 1
+	 *    fib(2) = 1
+	 *    fib(n) = fib(n-1) + fib(n-2)
+	 */
+	def fib(n: Int):Int={
+			if (n==1) 
+				1
+			else if (n==2) 
+				1
+			else
+				fib(n-1) + fib(n-2)
+	}
 
 }
